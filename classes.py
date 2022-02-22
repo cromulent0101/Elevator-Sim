1e39d19004746a7cee9a2f7d3223c79a9f6ac8ee
Joe Kaiser <kaiserfjoe@gmail.com>
2022-02-21 23:50:46 -0500

    Initial commit

---
+++ classes.py
@@ -0,0 +1,20 @@
+class Elevator(self, capacity:int): 
+    self.direction = 'up'
+    self.destination_queue = []
+    self.door_speed = 1
+    self.riders = []
+
+
+
+class Rider(self,name,destination:int):
+    self.name = name 
+    self.destination = destination
+
+class Floor(self,number:int):
+    self.number = number
+    self.riders = []
+    self.has_elevator = False
+    self.up_request = False
+    self.down_request = False
+
+
