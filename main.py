b3769a41c45c38caf6f2fceeed2a57daff8df6f4
Joe Kaiser <kaiserfjoe@gmail.com>
2022-02-22 23:03:13 -0500

    Add driver file

---
+++ main.py
@@ -0,0 +1,8 @@
+# create an elevator at bottom floor
+
+
+# create 60 floors
+
+# randomly create a bunch of riders on ground floor and a few on the top floors
+
+# have the riders call the elevator and try to get to their destination
