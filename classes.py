6027c17d66e2722572262aa6cb01a94356ff69c9
Joe Kaiser <kaiserfjoe@gmail.com>
2022-02-22 23:03:30 -0500

    Add functions for Elevator class

--- classes.py
+++ classes.py
@@ -1,14 +1,21 @@
-class Elevator(self, capacity:int): 
+class Elevator(self,capacity:int,floor:int): 
     self.direction = 'up'
     self.destination_queue = []
     self.door_speed = 1
+    self.elevator_speed = 10
+    self.is_moving = False
     self.riders = []
 
+    def add_rider(Rider):
+        self.riders.append(Rider)
 
+    def remove_rider(Rider):
+        self.riders.remove(Rider)
 
 class Rider(self,name,destination:int):
     self.name = name 
     self.destination = destination
+    self.curr_floor = 0
 
 class Floor(self,number:int):
     self.number = number
