2dacc9d2c11d0f2dd48028c4c5e7c87b7448e502
Joe Kaiser <kaiserfjoe@gmail.com>
2022-04-09 09:05:29 -0400

    Clean up comments

--- main.py
+++ main.py
@@ -40,12 +40,6 @@ while True:
         print('EOF!')
         break
 
-# # create all floors in between min and max # of the floors - one per Floor
-# print(f"creating floors {lowest_rider_floor} through {highest_rider_floor}")
-# floor_list = []
-# for floor in range(lowest_rider_floor,highest_rider_floor):
-#     floor_list.append(Floor(floor))
-
 # create an elevator at some floor
 e = Elevator(3,1)
 
@@ -66,13 +60,8 @@ while True:
     # update elevator's riders' floors
     for rider in e.riders:
         rider.curr_floor = e.floor
-    
-    print(f"rider_list is {len(rider_list)}")
-
-
 
 # I'm pretty sure I can combine the two following loops...
-    # see if anyone should get off, remove the destination from elevator destinations, and kill rider
     # if the current floor is a destinaton, we MUST stop
     # riders with that destination get off
     riders_to_remove = []
@@ -83,30 +72,28 @@ while True:
                 rider_list.remove(rider)
                 print(f"rider {rider.name} arrived at destination {rider.destination} and is at floor {e.floor}")
                 sleep(1)
-        e.destinations.remove(e.floor) # we can't remove a destination twice if it's someone's final floor and someone's starting floor
+        e.destinations.remove(e.floor) 
     for rider in riders_to_remove:
         e.riders.remove(rider)
-        
+
     if not rider_list: break
+    # we don't know the direction of the  static elevator until the new rider's destination is added
+
     if not e.destinations:
         e.direction = 0
-    # update elevator's direction if there are no more destinations higher/lower than current floor
-    # having this here solves case where elebator needs to turn around at the top,
-    # but fails to get the top person when every passenger wants to go only one up
 
-    # we don't know the direction of the elevator until the new rider's destination is added
-    # imple solution: concept of a stationary elevator
     # see if anyone needs to get on (in the elevator's direction), and add their destinations
     for rider in rider_list:
         if rider.start_floor == e.floor:
-            if (rider.destination > e.floor and e.direction > -1) or (rider.destination < e.floor and e.direction < 1): # this line is wrong
+            if (rider.destination > e.floor and e.direction > -1) or (rider.destination < e.floor and e.direction < 1): 
                 e.riders.append(rider)
                 if rider.start_floor in e.destinations:
-                    e.destinations.remove(rider.start_floor) # how to handle when one person gets off and another gets in/
+                    e.destinations.remove(rider.start_floor) 
                 e.destinations.add(rider.destination)
                 print(f"rider {rider.name} got on at floor {e.floor} going to {rider.destination}")
                 sleep(1)
-    
+
+    # update elevator's direction if there are no more destinations higher/lower than current floor
     if (all(e.floor > dest for dest in e.destinations) and e.direction>-1):
         e.direction = -1
         print(f"elevator reached floor {e.floor}, turned around, and is now going {e.direction}")
@@ -115,8 +102,6 @@ while True:
         e.direction = 1
         print(f"elevator reached floor {e.floor}, turned around, and is now going {e.direction}")
         sleep(1)
-
-    
 
     # at this point, doors close and we move again
     e.floor += e.direction
