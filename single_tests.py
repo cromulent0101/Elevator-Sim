3dc92957215d03e85a7006d12f885138ff421e61
Joe Kaiser <kaiserfjoe@gmail.com>
2022-09-08 09:17:50 -0400

    Rename tests for single elevator

--- single_tests.py
+++ single_tests.py
@@ -20,6 +20,13 @@ def middle_floor_elevator():
 
 
 @pytest.fixture
+def top_floor_elevator():
+    e = Elevator(3, 12)
+    e.direction = -1
+    return e
+
+
+@pytest.fixture
 def classic_riders():
     return [Rider("Joe", 9, 2), Rider("Bob", 5, 4), Rider("Jane", 1, 9)]
 
@@ -32,6 +39,17 @@ def ascending_riders():
 @pytest.fixture
 def descending_riders():
     return [Rider("Joe", 2, 9), Rider("Bob", 4, 5), Rider("Jane", 3, 7)]
+
+
+@pytest.fixture
+def multiple_riders_multiple_floors():
+    return [
+        Rider("Joe", 2, 9),
+        Rider("Bob", 2, 5),
+        Rider("Jane", 3, 5),
+        Rider("Jimmy", 5, 9),
+        Rider("Jill", 9, 5),
+    ]
 
 
 # @pytest.fixure(autouse=True)
@@ -111,3 +129,27 @@ def test_middle_classic(middle_floor_elevator, classic_riders):
         "8;1;;",
         "9;1;;Joe",
     ]
+
+
+def test_middle_multiple(middle_floor_elevator, multiple_riders_multiple_floors):
+    floor_dict = utils.create_floors(multiple_riders_multiple_floors)
+    assert middle_floor_elevator.run(multiple_riders_multiple_floors, floor_dict) == [
+        "3;1;;",
+        "4;1;;",
+        "5;1;Jill;",
+        "6;1;;",
+        "7;1;;",
+        "8;1;;",
+        "9;1;Jimmy,Joe;Jill",
+        "8;-1;;",
+        "7;-1;;",
+        "6;-1;;",
+        "5;-1;Bob,Jane;Jimmy",
+        "4;-1;;",
+        "3;-1;;Jane",
+        "2;-1;;Bob,Joe",
+    ]
+
+
+def test_middle_stationary_up():  # stationary elevator with
+    pass
