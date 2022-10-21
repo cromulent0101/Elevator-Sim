e080929f7606059cbd8e5c7b236706158c41f7bf
Joe Kaiser <kaiserfjoe@gmail.com>
2022-10-20 21:18:59 -0400

    Remove comments, add back second elev

--- main.py
+++ main.py
@@ -12,22 +12,12 @@ import concurrent.futures
 import tkinter as tk
 
 
-# rider_list = utils.get_riders()
-# rider_list = [
-#     Rider("Joe", 2, 9),
-#     Rider("Bob", 2, 5),
-#     Rider("Jane", 3, 5),
-#     Rider("Jimmy", 5, 9),
-#     Rider("Jill", 9, 5),
-# ]
 full_rider_list = utils.get_riders_from_csv("sims/sim1.csv")
-# create an elevator at a random floor
+
 e = Elevator(3, 12)
 t = Elevator(3, 3)
-# e.direction = -1
-t.direction = 1
 
-e_bank = [e]
+e_bank = [e, t]
 bank = ElevatorBank(e_bank)
 
 start_stop_delays = []
@@ -35,23 +25,9 @@ start_step_delays = []
 
 floor_dict = utils.create_floors(full_rider_list, e_bank)
 
-# arbitrarily choose elevator to go up first
 
 bank.simulate(full_rider_list, floor_dict)
-# e.elevate(rider_list, floor_dict, start_stop_delays, start_step_delays)
-# t1 = threading.Thread(
-#     target=e.elevate,
-#     args=[rider_list, floor_dict, start_stop_delays, start_step_delays],
-# )
-# t2 = threading.Thread(target=utils.update_riders, args=[rider_list, floor_dict])
 
-# t1.start()
-# sleep(2)
-# # t2.start()
-# t1.join()
-# # t2.join()
-# # output = e.elevate(rider_list, floor_dict)
-# # print(output)
 
 print(f"Average total wait: {mean(start_stop_delays)}")
 print(f"Median total wait: {median(start_stop_delays)}")
