ac993d4e4b24b062021e7b7e5e691ddf5c158881
Joe Kaiser <kaiserfjoe@gmail.com>
2022-12-20 22:15:32 -0500

    Change to nonrealtime simulate

--- main.py
+++ main.py
@@ -12,32 +12,34 @@ import concurrent.futures
 import tkinter as tk
 
 
-full_rider_list = utils.get_riders_from_csv("sims/edge_cases.csv")
+rider_list_csv = utils.get_riders_from_csv("sims/edge_cases.csv")
 
-e = Elevator(100, 80)
-t = Elevator(1, 3)
-z = Elevator(1, 1)
-q = Elevator(100, 60)
+e = Elevator(100, 80, "z")
+t = Elevator(1, 3, "t")
+z = Elevator(1, 1, "z")
+q = Elevator(100, 60, "q")
 
 e.direction = 0
 t.direction = 0
 z.direction = 0
 q.direction = 0
 
-e_bank = [t]
+e_bank = [t, z]
 bank = ElevatorBank(e_bank)
-
-
-floor_dict = utils.create_floors(full_rider_list, e_bank)
+floor_dict = utils.create_floors(rider_list_csv, e_bank)
+time_step = 0.01
+max_time = 10000
 
 
 start_step_delays, start_stop_delays, log_dict = bank.simulate(
-    full_rider_list, floor_dict
+    rider_list_csv, floor_dict, time_step, max_time
 )
 
 
+print(f"Overall total wait: {sum(start_stop_delays)}")
+print(f"Floor total wait: {sum(start_step_delays)}")
 print(f"Average total wait: {mean(start_stop_delays)}")
 print(f"Median total wait: {median(start_stop_delays)}")
 print(f"Average floor wait: {mean(start_step_delays)}")
-print(f"Median floor wait: {median(start_step_delays)}")
+print(f"Median floor wait : {median(start_step_delays)}")
 print(log_dict)
