f0d53bcc062a426e4d8de190f06b20c4c2f65b47
Joe Kaiser <kaiserfjoe@gmail.com>
2023-01-16 17:13:24 -0500

    Add main dunder and remove some imports

--- main.py
+++ main.py
@@ -1,84 +1,79 @@
-# TODO: add main dunder
 # pylint: disable=import-error
 from classes import Elevator, Rider, Floor, ElevatorBank
 import utils
 import csv
-from time import sleep
-from sys import maxsize
-from statistics import mean, median
 import random
-import threading
-import concurrent.futures
-import tkinter as tk
+from statistics import mean, median
 
 
-simluation_csv = "sims/classic.csv"
-rider_list_csv = utils.get_riders_from_csv(simluation_csv)
-rider_list_csv_floor = utils.get_riders_from_csv(simluation_csv)
+SIMULATION_CSV = "sims/classic.csv"
+ELEVATOR_CAPACITY = 1
+NUM_ELEVATORS = 3
+TIME_STEP = 0.01
+MAX_TIME = 10000
 
-elevator_capacity = 1
-num_elevators = 3
+rider_list_csv = utils.get_riders_from_csv(SIMULATION_CSV)
+rider_list_csv_floor = utils.get_riders_from_csv(SIMULATION_CSV)
 
-a = Elevator(elevator_capacity, 1, "a")
-b = Elevator(elevator_capacity, 1, "b")
-c = Elevator(elevator_capacity, 1, "c")
-d = Elevator(elevator_capacity, 1, "d")
-e = Elevator(elevator_capacity, 1, "e")
-f = Elevator(elevator_capacity, 1, "f")
-a_floor = Elevator(elevator_capacity, 1, "a_floor")
-b_floor = Elevator(elevator_capacity, 1, "b_floor")
-c_floor = Elevator(elevator_capacity, 1, "c_floor")
-d_floor = Elevator(elevator_capacity, 1, "d_floor")
-e_floor = Elevator(elevator_capacity, 1, "e_floor")
-f_floor = Elevator(elevator_capacity, 1, "f_floor")
+
+a = Elevator(ELEVATOR_CAPACITY, 1, "a")
+b = Elevator(ELEVATOR_CAPACITY, 1, "b")
+c = Elevator(ELEVATOR_CAPACITY, 1, "c")
+d = Elevator(ELEVATOR_CAPACITY, 1, "d")
+e = Elevator(ELEVATOR_CAPACITY, 1, "e")
+f = Elevator(ELEVATOR_CAPACITY, 1, "f")
+a_floor = Elevator(ELEVATOR_CAPACITY, 1, "a_floor")
+b_floor = Elevator(ELEVATOR_CAPACITY, 1, "b_floor")
+c_floor = Elevator(ELEVATOR_CAPACITY, 1, "c_floor")
+d_floor = Elevator(ELEVATOR_CAPACITY, 1, "d_floor")
+e_floor = Elevator(ELEVATOR_CAPACITY, 1, "e_floor")
+f_floor = Elevator(ELEVATOR_CAPACITY, 1, "f_floor")
 
 e_bank = [a, b, c, d, e, f]
 e_bank_floor = [a_floor, b_floor, c_floor, d_floor, e_floor, f_floor]
 
 
-bank = ElevatorBank(e_bank[:num_elevators])
-bank_floor = ElevatorBank(e_bank_floor[:num_elevators])
-floor_dict = utils.create_floors(rider_list_csv, e_bank[:num_elevators])
+bank = ElevatorBank(e_bank[:NUM_ELEVATORS])
+bank_floor = ElevatorBank(e_bank_floor[:NUM_ELEVATORS])
+floor_dict = utils.create_floors(rider_list_csv, e_bank[:NUM_ELEVATORS])
 floor_dict_floor = utils.create_floors(
-    rider_list_csv_floor, e_bank_floor[:num_elevators]
+    rider_list_csv_floor, e_bank_floor[:NUM_ELEVATORS]
 )
 
-time_step = 0.01
-max_time = 10000
+if __name__ == "__main__":
 
-start_step_delays, start_stop_delays, floors_traversed, log_dict = bank.simulate(
-    rider_list_csv, floor_dict, time_step, max_time, "elevate"
-)
+    start_step_delays, start_stop_delays, floors_traversed, log_dict = bank.simulate(
+        rider_list_csv, floor_dict, TIME_STEP, MAX_TIME, "elevate"
+    )
 
-(
-    start_step_delays_floor,
-    start_stop_delays_floor,
-    floors_traversed_floor,
-    log_dict_floor,
-) = bank_floor.simulate(
-    rider_list_csv_floor, floor_dict_floor, time_step, max_time, "elevate_floor"
-)
+    (
+        start_step_delays_floor,
+        start_stop_delays_floor,
+        floors_traversed_floor,
+        log_dict_floor,
+    ) = bank_floor.simulate(
+        rider_list_csv_floor, floor_dict_floor, TIME_STEP, MAX_TIME, "elevate_floor"
+    )
 
-
-print(log_dict_floor)
-print(log_dict)
-print(f"Overall total wait: {sum(start_stop_delays)}")
-print(f"Overall total wait dispatch: {sum(start_stop_delays_floor)}")
-print("")
-print(f"Floor total wait: {sum(start_step_delays)}")
-print(f"Floor total wait dispatch: {sum(start_step_delays_floor)}")
-print("")
-print(f"Average total wait: {mean(start_stop_delays)}")
-print(f"Average total wait dispatch: {mean(start_stop_delays_floor)}")
-print("")
-print(f"Median total wait: {median(start_stop_delays)}")
-print(f"Median total wait dispatch: {median(start_stop_delays_floor)}")
-print("")
-print(f"Average floor wait: {mean(start_step_delays)}")
-print(f"Average floor wait dispatch: {mean(start_step_delays_floor)}")
-print("")
-print(f"Median floor wait: {median(start_step_delays)}")
-print(f"Median floor wait dispatch: {median(start_step_delays_floor)}")
-print("")
-print(f"Floors traversed: {floors_traversed}")
-print(f"Floors traversed by dispatch: {floors_traversed_floor}")
+    print(log_dict_floor)
+    print(log_dict)
+    print(f"Overall total wait: {sum(start_stop_delays)}")
+    print(f"Overall total wait dispatch: {sum(start_stop_delays_floor)}")
+    print("")
+    print(f"Floor total wait: {sum(start_step_delays)}")
+    print(f"Floor total wait dispatch: {sum(start_step_delays_floor)}")
+    print("")
+    print(f"Average total wait: {mean(start_stop_delays)}")
+    print(f"Average total wait dispatch: {mean(start_stop_delays_floor)}")
+    print("")
+    print(f"Median total wait: {median(start_stop_delays)}")
+    print(f"Median total wait dispatch: {median(start_stop_delays_floor)}")
+    print("")
+    print(f"Average floor wait: {mean(start_step_delays)}")
+    print(f"Average floor wait dispatch: {mean(start_step_delays_floor)}")
+    print("")
+    print(f"Median floor wait: {median(start_step_delays)}")
+    print(f"Median floor wait dispatch: {median(start_step_delays_floor)}")
+    print("")
+    print(f"Floors traversed: {floors_traversed}")
+    print(f"Floors traversed by dispatch: {floors_traversed_floor}")
