b7b117f714d679f432085dd895773dfdbf681ffb
Joe Kaiser <kaiserfjoe@gmail.com>
2023-02-16 13:54:37 -0500

    Add assertion that Rider name occurs twice in log

--- tests/normal_single_tests_csv.py
+++ tests/normal_single_tests_csv.py
@@ -66,10 +66,11 @@ def random100_at_once_riders():
     return utils.get_riders_from_csv("./sims/100random_at_once.csv")
 
 
-# TODO: add actual assert statements here
 def test_first_classic(
     first_floor_elevator, classic_riders, simulation_settings_normal
 ):
+
+    classic_riders_copy = classic_riders.copy()
     e_bank = [first_floor_elevator]
     bank = ElevatorBank(e_bank)
     floor_dict = utils.create_floors(classic_riders, e_bank)
@@ -81,14 +82,16 @@ def test_first_classic(
         simulation_settings_normal[1],
         simulation_settings_normal[2],
     )
-    # print(classic_riders[0])
-    # print(log_dict.values[0])
-    # assert classic_riders[0].name in ''.join(log_dict.values[0])
+
+    assert (
+        "".join(list(log_dict.values())[0]).count(classic_riders_copy[0].name) == 2
+    )  # assumes Rider names are prefix-free
 
 
 def test_random100(
     first_floor_elevator, random100_at_once_riders, simulation_settings_normal
 ):
+    random100_at_once_riders_copy = random100_at_once_riders.copy()
     e_bank = [first_floor_elevator]
     bank = ElevatorBank(e_bank)
     floor_dict = utils.create_floors(random100_at_once_riders, e_bank)
@@ -100,3 +103,8 @@ def test_random100(
         simulation_settings_normal[1],
         simulation_settings_normal[2],
     )
+
+    assert (
+        "".join(list(log_dict.values())[0]).count(random100_at_once_riders_copy[0].name)
+        == 2
+    )  # assumes Rider names are prefix-free
