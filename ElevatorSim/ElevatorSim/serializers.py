2050be497b0bd1c792f9a6de3aee61c8bbc8e2e1
Joe Kaiser <kaiserfjoe@gmail.com>
2023-02-23 23:29:14 -0500

    Add unfinished logic to run sim from request data

--- ElevatorSim/ElevatorSim/serializers.py
+++ ElevatorSim/ElevatorSim/serializers.py
@@ -1,6 +1,10 @@
 # code taken from https://www.digitalocean.com/community/tutorials/build-a-to-do-application-using-django-and-react
 # pylint: disable=import-error
-
+import utils
+import csv
+import random
+import os
+from classes import Elevator, Rider, Floor, ElevatorBank
 from rest_framework import serializers
 from .models import Simulation, SimulationRequest
 
@@ -30,3 +34,52 @@ class SimulationRequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = SimulationRequest
         fields = ("rider_list", "num_elevators", "TIME_STEP", "elevate_type")
+
+    def create(self, validated_data):
+        # hardcode 6 elevs for now with 3 capacity
+        a = Elevator(3, 1, "a")
+        b = Elevator(3, 1, "b")
+        c = Elevator(3, 1, "c")
+        d = Elevator(3, 1, "d")
+        e = Elevator(3, 1, "e")
+        f = Elevator(3, 1, "f")
+
+        e_bank = [a, b, c, d, e, f]
+
+        # this should be split out
+        rider_list = []
+        rider_input = validated_data.pop(
+            "rider_list"
+        )  # can we eventually push back to validated_data?
+        for line in rider_input:
+            rider_data = line.split(";")
+            new_rider = Rider(rider_data[0], int(rider_data[1]), int(rider_data[2]))
+            new_rider.when_to_add = int(rider_data[3])
+            rider_list.append(new_rider)
+
+        NUM_ELEVATORS = validated_data.pop("num_elevators")
+        bank = ElevatorBank(e_bank[:NUM_ELEVATORS])
+        floor_dict = utils.create_floors(rider_list, e_bank[:NUM_ELEVATORS])
+
+        (
+            start_step_delays,
+            start_stop_delays,
+            floors_traversed,
+            log_dict,
+        ) = bank.simulate(
+            rider_list,
+            floor_dict,
+            TIME_STEP,
+            MAX_TIME,
+            validated_data.pop("elevate_type"),
+        )
+
+        sim = SimulationRequest.objects.create(
+            rider_list="asdf",
+            num_elevators="ASDASDASDASD",
+            TIME_STEP=0.1,
+            elevate_type="hello",
+        )
+        # parent = super().create(**validated_data)
+
+        return sim
